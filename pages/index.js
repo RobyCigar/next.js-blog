12361e673a682842b6b665091dd37e056020baa1
rabih <rabihutomo11@gmail.com>
2020-12-29 10:13:08 +0700

    add routes, api etc

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,9 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData} from '../lib/posts'
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
 
 export default function Home({allPostsData}) {
   return (
@@ -41,4 +34,13 @@ export default function Home({allPostsData}) {
       </section>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
